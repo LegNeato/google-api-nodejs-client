20039324f1da3c95197f79c7e7671bcc4ca9bc1e
Christian Legnitto <christian@legnitto.com>
2016-11-02 23:40:04 -0700

    Add trailing commas to output

--- templates/paths.js
+++ templates/paths.js
@@ -9,7 +9,7 @@ export type PathMapping = {
 
 const mapping: Array<PathMapping> = [
 {% for rname, r in resources %}{% for mname, m in r.methods %}
-  { method: '{{ m.httpMethod }}', handler: '{{ m.id }}', path: '{{ (servicePath + m.path)|buildurl|toExpressPath }}'},
+  { method: '{{ m.httpMethod }}', handler: '{{ m.id }}', path: '{{ (servicePath + m.path)|buildurl|toExpressPath }}',},
 {%- endfor -%}{%- endfor -%}
 ];
 
